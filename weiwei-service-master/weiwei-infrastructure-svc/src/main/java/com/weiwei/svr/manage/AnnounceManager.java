9e745205f397f2adc7bf700b9fe49b0abe7fc9f4
yktang <tyingkai@gmail.com>
2015-04-28 20:22:18 +0800

    Remove

--- weiwei-service-master/weiwei-infrastructure-svc/src/main/java/com/weiwei/svr/manage/AnnounceManager.java
+++
@@ -1,8 +0,0 @@
-package com.weiwei.svr.manage;
-
-import java.util.List;
-
-public interface AnnounceManager {
-	public List<?> findBySequenceId(int startId, int endId);
-	public List<?> findByUrl(String url);
-}
--- weiwei-service-master/weiwei-infrastructure-svc/src/main/java/com/weiwei/svr/utils/SVRFieldValidator.java
+++
@@ -1,7 +0,0 @@
-package com.weiwei.svr.utils;
-
-public class SVRFieldValidator {
-	public static boolean isNullOrBlank(String fieldName){
-		return ((null == fieldName) || (0 == fieldName.trim().length()));
-	}
-}
